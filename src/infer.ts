22e68e35c20094e2b6ed879423976846126ea06c
宋均均 <songjunjun@bytedance.com>
2021-06-10 14:46:49 +0800

    feat: add

--- src/infer.ts
+++ src/infer.ts
@@ -23,5 +23,21 @@ type typeofType = typeof Function;
 type _ConstructorParameters<T extends new (...args: any) => any> = T extends new (...args: infer A) => any ? A : never
 type constructorParameters = _ConstructorParameters<typeof School>
 
-type _ThisParameterType<T> = T extends (this: infer U, ...args: any) => any ? U : unknown;
-type thisParameterType = _ThisParameterType<typeof Function>
+type _ThisParameterType<T> = T extends (this: infer U, ...args: any[]) => any ? U : unknown;
+function toHex(this: number) {
+  return this.toString(16);
+}
+type thisParameterType = _ThisParameterType<typeof toHex>
+function numberToString(n: thisParameterType) {
+  return toHex.apply(n);
+}
+
+type _OmitThisParameter<T> =
+// 不包含this参数, 则直接返回
+  unknown extends _ThisParameterType<T>
+    ? T : T extends (...args: infer A) => infer R
+    ? (...args: A) => R : T;
+function add(this: number, b: string) {
+  return this + +b;
+}
+type returnNumber = _OmitThisParameter<typeof add>
